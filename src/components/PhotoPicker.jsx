f88a0ed48645c8eab09809b176395e300049b747
“Mohammad <firmansmoh842@gmail.com>
2023-10-08 12:31:41 +0700

    add new component for photo picker

---
+++ src/components/PhotoPicker.jsx
@@ -0,0 +1,41 @@
+import dummy from '../assets/dummy.png'
+
+export default function PhotoPicker(){
+    return (
+        <div>
+
+            <div className="circle">
+                <img className="photo" id="photo-pre-img" src={dummy} alt="dummy photo" width="158px" height="152px"/>
+
+                
+                <label htmlFor="photo">
+
+                    <a id="btn-add-photo" className='btn-add-photo'>+</a>
+                </label>
+
+                <input type="file" onChange={(e) => upPhoto(e)} name="photo" id="photo" className='photo-file' />
+            </div>
+        </div>
+    )
+}
+
+function upPhoto(e) {
+    console.log(e.target.files[0]);
+
+    var files = e.target.files
+     // FileReader support
+    if (FileReader && files && files.length) {
+        var fr = new FileReader();
+        fr.onload = function () {
+            document.getElementById("photo-pre-img").src = fr.result;
+        }
+        fr.readAsDataURL(files[0]);
+    }
+    
+    // Not supported
+    else {
+        // fallback -- perhaps submit the input to an iframe and temporarily store
+        // them on the server until the user's session ends.
+    }
+
+}
--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,11 +1,14 @@
 import { NavLink } from "react-router-dom"
 import NavTop from "../components/NavTop"
 import InputText from "../components/InputText"
+import PhotoPicker from "../components/PhotoPicker"
 export default function Create(){
     return (
         <div>
             <NavTop title="Tambah Anak"/>
 
+
+            <PhotoPicker/>
             <InputText name="name" placeholder="Masukan nama anak anda"/>
         </div>
     )
