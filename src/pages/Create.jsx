a898cac1de97fceef0d3eed54eb18c59fa37453d
“Mohammad <firmansmoh842@gmail.com>
2023-10-11 18:59:29 +0700

    add create page

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,15 +1,28 @@
 import { NavLink } from "react-router-dom"
 import NavTop from "../components/NavTop"
-import InputText from "../components/InputText"
+import Input from "../components/Input"
 import PhotoPicker from "../components/PhotoPicker"
+import Button from "../components/Button"
+
 export default function Create(){
     return (
         <div>
             <NavTop title="Tambah Anak"/>
 
-
             <PhotoPicker/>
-            <InputText name="name" placeholder="Masukan nama anak anda"/>
+            
+            <br />
+            <Input name="name" placeholder="Masukan nama anak anda"/>
+            <Input name="anakke" placeholder="Anak ke-"/>
+            <Input name="tgllahir" placeholder="Tanggal Lahir" type="date"/>
+            <Input name="tempatlahir" placeholder="Tempat Lahir"/>
+            <Input name="gender" placeholder="Jenis Kelamin"/>
+            <Input name="bobot" placeholder="Bobot"/>
+            <Button  placeholder={"+ Tambah Anak"} click={clicked} mode="main"/>
         </div>
     )
+}
+
+function clicked(params) {
+    alert("clicked")
 }
